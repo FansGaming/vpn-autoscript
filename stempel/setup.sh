771741da775f70562f4f1bdea16592d9bd315f91
FansGaming <114886753+FansGaming@users.noreply.github.com>
2024-12-31 04:56:04 -0800

    Update setup.sh

--- stempel/setup.sh
+++ stempel/setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-# cari apa 
-# harta tahta hanya sementara ingat masih ada kehidupan setelah kematian
-# jangan lupa sholat
+
 rm -rf setup.sh
 rm -rf /var/lib/ipvps.conf
 rm -rf /root/scdomain
@@ -25,7 +23,7 @@ yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
 green() { echo -e "\\033[32;1m${*}\\033[0m"; }
 red() { echo -e "\\033[31;1m${*}\\033[0m"; }
 # domain random
-#CDN="https://raw.githubusercontent.com/givpn/AutoScriptXray/master/ssh"
+#CDN="https://raw.githubusercontent.com/FansGaming/vpn-autoscript/main/ssh"
 cd /root
 #System version number
 if [ "${EUID}" -ne 0 ]; then
@@ -154,15 +152,15 @@ echo -e "$BGreen      Install SSH Websocket           $NC"
 echo -e "\e[33m-----------------------------------\033[0m"
 sleep 0.5
 clear
-wget https://raw.githubusercontent.com/givpn/AutoScriptXray/master/ssh/ssh-vpn.sh && chmod +x ssh-vpn.sh && ./ssh-vpn.sh
+wget https://raw.githubusercontent.com/FansGaming/vpn-autoscript/main/ssh/ssh-vpn.sh && chmod +x ssh-vpn.sh && ./ssh-vpn.sh
 #Instal Xray
 echo -e "\e[33m-----------------------------------\033[0m"
 echo -e "$BGreen          Install XRAY              $NC"
 echo -e "\e[33m-----------------------------------\033[0m"
 sleep 0.5
 clear
-wget https://raw.githubusercontent.com/givpn/AutoScriptXray/master/xray/ins-xray.sh && chmod +x ins-xray.sh && ./ins-xray.sh
-wget https://raw.githubusercontent.com/givpn/AutoScriptXray/master/sshws/insshws.sh && chmod +x insshws.sh && ./insshws.sh
+wget https://raw.githubusercontent.com/FansGaming/vpn-autoscript/main/xray/ins-xray.sh && chmod +x ins-xray.sh && ./ins-xray.sh
+wget https://raw.githubusercontent.com/FansGaming/vpn-autoscript/main/sshws/insshws.sh && chmod +x insshws.sh && ./insshws.sh
 clear
 cat> /root/.profile << END
 # ~/.profile: executed by Bourne-compatible login shells.
@@ -201,7 +199,7 @@ if [ ! -f "/etc/log-create-shadowsocks.log" ]; then
 echo "Log Shadowsocks Account " > /etc/log-create-shadowsocks.log
 fi
 history -c
-serverV=$( curl -sS https://raw.githubusercontent.com/givpn/AutoScriptXray/master/menu/versi  )
+serverV=$( curl -sS https://raw.githubusercontent.com/FansGaming/vpn-autoscript/main/menu/versi  )
 echo $serverV > /opt/.ver
 aureb=$(cat /home/re_otm)
 b=11
