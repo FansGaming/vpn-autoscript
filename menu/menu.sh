7446c94a28fb76c71185a71aefb21b2d6b860db5
FansGaming <114886753+FansGaming@users.noreply.github.com>
2024-12-31 06:54:01 -0800

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -81,7 +81,8 @@ echo -e "\e[1;33m -------------------------------------------------\e[0m"
 echo -e "\e[1;32m OS            \e[0m: "`hostnamectl | grep "Operating System" | cut -d ' ' -f5-`	
 echo -e "\e[1;32m Uptime        \e[0m: $uptime"
 echo -e "\e[1;32m Public IP     \e[0m: $IPVPS"
-echo -e "\e[1;32m Country       \e[0m: $LOC"
+LOC=$(curl -s http://ip-api.com/json/ | jq -r '.country')
+echo -e "\e[1;32mCountry        \e[0m: $LOC"
 #echo -e "\e[1;32m ASN           \e[0m: $ISP"
 #echo -e "\e[1;32m CITY          \e[0m: $CITY"
 echo -e "\e[1;32m DOMAIN        \e[0m: $domain"	
