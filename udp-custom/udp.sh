752d942efcd95e677470945eb0d4870b5d969ae9
FansGaming <114886753+FansGaming@users.noreply.github.com>
2024-12-31 05:21:21 -0800

    Update udp.sh

--- udp-custom/udp.sh
+++ udp-custom/udp.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-# Script UdpCustom 2023
-# Script By MARDHEX
-# https://t.me/mdxdev
+# Script UdpCustom 2024
 # =========================================
 # Quick Setup | Script Setup Manager
-# Edition : Stable Edition 1.0
-# Auther  : givpn
-# Kuota Habisss
-# (C) Copyright 2023
+# Edition : Edition 2.0 beta
+# Auther  : Administrator
+# (C) Copyright 2024
 # =========================================
 # pewarna hidup
 BGreen='\e[1;32m'
@@ -23,13 +20,13 @@ rm -rf /usr/bin/trial
 rm -rf /root/domain
 echo "\e[1;32m Update Menu.. \e[0m"
 sleep 1
-wget -q -O /usr/bin/usernew https://raw.githubusercontent.com/givpn/AutoScriptXray/master/udp-custom/ssh/usernew.sh
-wget -q -O /usr/bin/trial https://raw.githubusercontent.com/givpn/AutoScriptXray/master/udp-custom/ssh/trial.sh
+wget -q -O /usr/bin/usernew https://raw.githubusercontent.com/FansGaming/vpn-autoscript/main/udp-custom/ssh/usernew.sh
+wget -q -O /usr/bin/trial https://raw.githubusercontent.com/FansGaming/vpn-autoscript/main/udp-custom/ssh/trial.sh
 echo "\e[1;32m Proses Download Script Slowdns.. \e[0m"
-wget https://raw.githubusercontent.com/givpn/AutoScriptXray/master/udp-custom/slowdns/slowdns.sh && chmod +x slowdns.sh && ./slowdns.sh
+wget https://raw.githubusercontent.com/FansGaming/vpn-autoscript/main/udp-custom/slowdns/slowdns.sh && chmod +x slowdns.sh && ./slowdns.sh
 sleep 1
 echo "\e[1;32m Proses Download Script OpenVPN.. \e[0m"
-wget https://raw.githubusercontent.com/givpn/AutoScriptXray/master/udp-custom/openvpn/openvpn.sh && chmod +x openvpn.sh && ./openvpn.sh
+wget https://raw.githubusercontent.com/FansGaming/vpn-autoscript/main/udp-custom/openvpn/openvpn.sh && chmod +x openvpn.sh && ./openvpn.sh
 sleep 1
 chmod +x /usr/bin/usernew
 chmod +x /usr/bin/trial
@@ -50,7 +47,7 @@ clear
 echo "\e[1;32m Please Waiting Proses Downloading Toll UdpCustom.. \e[0m"
 sleep 1
 clear
-wget -q --show-progress --load-cookies /tmp/cookies.txt "https://github.com/givpn/AutoScriptXray/raw/master/udp-custom/udp-custom-linux-amd64" -O /root/udp/udp-custom && rm -rf /tmp/cookies.txt
+wget -q --show-progress --load-cookies /tmp/cookies.txt "https://raw.githubusercontent.com/FansGaming/vpn-autoscript/main/udp-custom/udp-custom-linux-amd64" -O /root/udp/udp-custom && rm -rf /tmp/cookies.txt
 chmod +x /root/udp/udp-custom
 clear
 # install Config Default Udp
@@ -68,7 +65,7 @@ clear
 echo "\e[1;32m Please Waiting Proses Downloading Default Config UdpCustom.. \e[0m"
 sleep 1
 clear
-wget -q --show-progress --load-cookies /tmp/cookies.txt "https://github.com/givpn/AutoScriptXray/raw/master/udp-custom/config.json" -O /root/udp/config.json && rm -rf /tmp/cookies.txt
+wget -q --show-progress --load-cookies /tmp/cookies.txt "https://raw.githubusercontent.com/FansGaming/vpn-autoscript/main/udp-custom/config.json" -O /root/udp/config.json && rm -rf /tmp/cookies.txt
 chmod 644 /root/udp/config.json
 
 if [ -z "$1" ]; then
