fcebaba920f2a15bc4f698a08f132a01a83d482e
FansGaming <114886753+FansGaming@users.noreply.github.com>
2024-12-31 22:07:59 -0800

    Update m-system.sh

--- menu/m-system.sh
+++ menu/m-system.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 # Quick Setup | Script Setup Manager
-# Edition : Stable Edition 1.0
-# Author  : givps
-# The MIT License (MIT)
-# (C) Copyright 2023
+# Edition : Edition 2.0 beta
+# Auther  : Administrator
+# (C) Copyright 2024
 # =========================================
 MYIP=$(wget -qO- ipv4.icanhazip.com);
 echo "Checking VPS"
